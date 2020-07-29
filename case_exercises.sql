89cd9176df38aaa4ad8ba6917405944035db5cd0
Yongliang Shi <yongliang.michael.shi@gmail.com>
2020-07-28 21:58:06 -0500

    review case exercise

--- case_exercises.sql
+++ case_exercises.sql
@@ -4,15 +4,17 @@
 use employees;
 
 select ee.emp_no
-				, de.dept_no
-				, de.from_date
-				, de.to_date
-				, case when de.to_date > curdate() then 1
-					when de.to_date < curdate() then 0
-					else 'Unclear'
-					end as is_current_employee
+		, de.dept_no
+		, de.from_date
+		, de.to_date
+		, case when de.to_date > curdate() then 1
+		when de.to_date < curdate() then 0
+		else 'Unclear'
+		end as is_current_employee
 from employees as ee
 join dept_emp as de on ee.emp_no = de.emp_no;
+-- The output have duplicates (331603). A subquery or temporary table would solve the issue.
+-- The data is not huge so temporary 
 
 -- Q2: Write a query that returns all employee names, and a new column 'alpha_group' that returns 'A-H', 'I-Q', or 'R-Z' 
 -- depending on the first letter of their last name.
@@ -23,10 +25,12 @@ select concat(first_name,' ', last_name) as full_name
 				    when substr(last_name,1,1) in ('R','S','T','U','V','W','X','Y','Z') then 'R-Z'
 				    end AS alpha_group
 from employees;
+-- There is a easy way: last_name REGEXP '^[A-H]' Then 'A-H'
+-- Another easy way: < 'I', < 'R', > 'Q'
 
 -- Q3: How many employees were born in each decade?
 
-select decade, count(decade) 
+select decade, count(decade)
 from (
     select *
 		, case when birth_date between '1950-01-01' and '1959-12-31' then '1950s'
@@ -40,6 +44,15 @@ from (
     order by birth_date
 ) as each_dec
 group by each_dec.decade
+
+-- Another approach
+
+Select sum(case when year(birth_date) >= 1950 and birth_date < 1960 then 1 else null end) as fifties,
+	sum(case when year(birth_date) >= 1960 and birth_date < 1970 then 1 else null end) as sixties,
+from employees
+
+-- Another approach
+-- substr
 
 -- BONUS
 -- What is the average salary for each of the following department groups: R&D, Sales & Marketing, Prod & QM, Finance & HR, Customer Service?
@@ -59,25 +72,7 @@ join employees.dept_emp as de on dep.dept_no = de.dept_no and de.to_date > curda
 join employees.salaries as sa on de.emp_no = sa.emp_no and sa.to_date > curdate()
 group by dep.dept_name;
 
-
-use employees;
-
-drop table dept_combo_salary;
-
-create temporary table dept_combo_salary as
-select dep.dept_name, avg(sa.salary) as dept_avg_sa
-from employees.departments as dep
-join employees.dept_emp as de on dep.dept_no = de.dept_no and de.to_date > curdate()
-join employees.salaries as sa on de.emp_no = sa.emp_no and sa.to_date > curdate()
-group by dep.dept_name;
-
-select *
-from dept_combo_salary;
-
--- Add column for combined departments
-alter table dept_combo_salary drop column dept_group;
-alter table dept_combo_salary drop column avg_salary;
-
+-- Add column for combined department
 alter table dept_combo_salary add dept_group varchar(20) default 'NUll';
 alter table dept_combo_salary add avg_salary float default 0;
 /* insert into dept_combo_salary(avg_salary) values (1),(2),(3),(4),(5),(6),(7),(8),(9); */
@@ -105,4 +100,4 @@ end as avg_salary
 
 from dept_combo_salary;
 
-
+-- Then I lost. The approach I took was too complicated.
